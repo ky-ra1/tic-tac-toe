49a2007f65cdab8156d5a5d603b98c006cedc005
ky-ra1 <chayahkyra@gmail.comm>
2021-11-27 14:40:14 +1100

    added images and updated files

--- README.md
+++ README.md
@@ -3,18 +3,26 @@
 URL for project: https://ky-ra1.github.io/tic-tac-toe/
 
 # Task
-Create a game of Tic-Tac-toe with HTML, CSS and Javascript.
+Create a game of Tic-Tac-Toe with HTML, CSS and Javascript.
 
 
 # Technologies Used
 For this project I used:
+- HTML
+- CSS 
+- Javascript
+- Github 
+- Gitpages
+- VSCode
+- Draw.io
+- Wireframe.cc 
 
+# Wireframe Drawings and Logic
+URL: https://wireframe.cc/CU8Pbg
 
-# Approach Taken 
-
-
-# Installation instructions
-
-
-
+Game Design and Logic:
+![logic](images/gamelogic.png)
 # Unsolved Problems
+Next update I will try to implement a 'reset scoreboard' option, as it was not working.
+
+Will also add an instructions section to the page.
--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-// user choice and grid
+// users and grid
 let user1 = "X";
 let user2 = "O";
 let currentUser = user1;
 let gridBox = ['', '', '', '', '', '', '', '', ''];
-let isWinner = false;
+let scoreboard = {
+    user1: 0,
+    user2: 0,
+    ties: 0,
+};
 
-
-const grid = document.querySelector(`.grid`);
-grid.addEventListener('click', function (event) {
+// click handler //
+function cellClick(event) {
     if (event.target.classList.contains('box')) {
         let id = event.target.id;
         if (gridBox[id] == "") {
@@ -18,9 +21,16 @@ grid.addEventListener('click', function (event) {
             checkGrid();
             switchUser();
         }
-    } console.log(gridBox);
-});
+    }
+}
 
+// checking to see if grid boxes are empty //
+const grid = document.querySelector(`.grid`);
+grid.addEventListener('click', cellClick);
+console.log(gridBox);
+
+
+// switching users //
 function switchUser() {
     if (currentUser == user1) {
         currentUser = user2;
@@ -30,6 +40,8 @@ function switchUser() {
 }
 console.log(gridBox[0]);
 
+
+// checking grid for winning combinations//
 function checkGrid() {
     if (gridBox[0] == gridBox[1] && gridBox[1] == gridBox[2] && gridBox[2] !== "") {
         console.log('winner horizontal top row') // winning option no 4 //
@@ -55,29 +67,92 @@ function checkGrid() {
     } else if (gridBox[0] == gridBox[4] && gridBox[4] == gridBox[8] && gridBox[8] !== "") {
         console.log('winner diagonal 2') // winning option no 8 //
         displayWinner();
+    } else if (!gridBox.includes("")) {
+        drawGame();
     }
-}
+};
 
+// displays tie and removes event listener//
+function drawGame() {
+    let draw = document.querySelector(`.draw`)
+    scoreboard.ties = scoreboard.ties + 1;
+    draw.textContent = `It's a tie!`;
+    alert('You have tied!')
+    const grid = document.querySelector(`.grid`);
+    grid.removeEventListener('click', cellClick);
+    updateScoreboard();
+};
+
+
+// displaying winner and removes event listener //
 function displayWinner() {
-    let winnerBox = document.querySelector('.winner');
-    winnerBox.textContent = `${currentUser} wins!!`;
-    isWinner = true;
-    alert("You are the winner");
+    let winnerBox = document.querySelector('.win');
+    if (currentUser == user1) {
+        scoreboard.user1 = scoreboard.user1 + 1
+    } else {
+        scoreboard.user2 = scoreboard.user2 + 1
+    }
+    winnerBox.textContent = `${currentUser} is the winner!`;
+    alert('You have won!')
+    const grid = document.querySelector(`.grid`);
+    grid.removeEventListener('click', cellClick);
+    updateScoreboard();
+};
+
+
+// updating scoreboard //
+function updateScoreboard() {
+    let xScore = document.getElementById("x-score")
+    xScore.textContent = scoreboard.user1;
+    let oScore = document.getElementById("o-score")
+    oScore.textContent = scoreboard.user2;
+    let tieScore = document.getElementById("tie-score")
+    tieScore.textContent = scoreboard.ties;
 }
 
+// resetting game //
 let reset = document.querySelector(`.reset`);
-reset.addEventListener('click', function (event) {
-    if (isWinner == true) {
-        isWinner = false;
-        gridBox = ['', '', '', '', '', '', '', '', ''];
-        currentPlayer = user1;
-        let boxes = document.querySelectorAll('.box');
-        for (let i = 0; i < boxes.length; i++) {
-            boxes[i].style.backgroundColor = "rgb(245, 9, 213)";
-            boxes[i].textContent = "";
-        }
+reset.addEventListener('click', function () {
+    let winnerBox = document.querySelector('.win');
+    winnerBox.textContent = "";
+    let draw = document.querySelector(`.draw`)
+    draw.textContent = "";
+    gridBox = ['', '', '', '', '', '', '', '', ''];
+    currentUser = user1;
+    let boxes = document.querySelectorAll('.box');
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].style.backgroundColor = "rgb(245, 9, 213)";
+        boxes[i].textContent = "";
     }
+    const grid = document.querySelector(`.grid`);
+    grid.addEventListener('click', cellClick);
 });
 
 
-//need to do tie 
+
+// eventually want to update to add reset scoreboard //
+// resetting scoreboard//
+// let resetScoreboard = document.querySelector(`.reset-scoreboard`);
+// resetScoreboard.addEventListener('click', function () {
+    // let xScoreReset = document.querySelector('.x-score')
+    // xScoreReset.TEXT_NODE = "";
+    // let oScoreReset = document.querySelector('.o-score')
+    // oScoreReset.TEXT_NODE = "";
+    // let tieScoreReset = document.querySelector('.tie-score')
+    // tieScoreReset.TEXT_NODE = "";
+    // let resetScoreAll = document.querySelector('.scoreboard');
+    // for (let i = 0; i < resetScoreAll.length; i++) {
+    // scoreboard[i] = "";
+    // }     // const grid = document.querySelector(`.grid`);
+    // grid.addEventListener('click', cellClick);
+// });
+
+// function resetSB() {
+    // let xScore = document.getElementById("x-score")
+    // xScore = scoreboard.user1 = "";
+    // let oScore = document.getElementById("o-score")
+    // oScore = scoreboard.user2 = "";
+    // let tieScore = document.getElementById("tie-score")
+    // tieScore = scoreboard.ties = "";
+// }
+// resetSB();
